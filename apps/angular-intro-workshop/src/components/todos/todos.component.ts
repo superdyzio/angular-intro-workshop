0b6271b8506ccd4542f36c2a147574055e235d0f
Dawid Perdek <perdekdawid@gmail.com>
2024-11-07 13:07:22 +0100

    filtering todos using rxjs with debounce

--- apps/angular-intro-workshop/src/components/todos/todos.component.ts
+++ apps/angular-intro-workshop/src/components/todos/todos.component.ts
@@ -1,9 +1,10 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, inject, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ApiService } from '../../services/api.service';
 import { Todo } from '../../model/todo.types';
 import { FormsModule } from '@angular/forms';
 import { FilterPipe } from '../../pipes/filter.pipe';
+import { BehaviorSubject, debounceTime, fromEvent, map, startWith } from 'rxjs';
 
 @Component({
   selector: 'app-todos',
@@ -12,14 +13,32 @@ import { FilterPipe } from '../../pipes/filter.pipe';
   providers: [ApiService],
   templateUrl: './todos.component.html',
 })
-export class TodosComponent implements OnInit {
+export class TodosComponent implements OnInit, AfterViewInit {
   apiService = inject(ApiService);
   todos: Todo[] = [];
   searchText = '';
+  visibleTodosSubject$ = new BehaviorSubject(this.todos);
+  visibleTodos$ = this.visibleTodosSubject$.asObservable();
+
+  @ViewChild('search')
+  searchInput: ElementRef | undefined;
 
   ngOnInit(): void {
-    this.apiService.getTodos().subscribe(todos => {
-      this.todos = todos
+    this.apiService.getTodos().subscribe((todos) => {
+      this.todos = todos;
+      this.visibleTodosSubject$.next(todos);
     });
+  }
+
+  ngAfterViewInit(): void {
+    fromEvent(this.searchInput?.nativeElement, 'input').pipe(
+      debounceTime(500),
+      map((event: any) => (event.target as HTMLInputElement).value.trim()),
+      map((searchText) =>
+        this.todos.filter((todo) => todo.title.includes(searchText))
+      ),
+      startWith(this.todos)
+    )
+      .subscribe(visibleTodos => this.visibleTodosSubject$.next(visibleTodos));
   }
 }
